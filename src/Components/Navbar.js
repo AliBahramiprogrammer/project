61c3cee2cc50a504d6822e8b9b30b21b21c558c5
Ali <abahrami.business@gmail.com>
2023-03-08 15:33:01 +0330

    Add Project

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="./Images/Troll-Face.png" alt="img" />
+      <h2>Meme Generator</h2>
+      <h3>React Course - Project 3</h3>
+    </nav>
+  )
+}
